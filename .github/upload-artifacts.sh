2743419506f169e0a3cf4c509dff88ca0192e8ec
nlkomaru <nikomaru@nikomaru.dev>
2025-06-11 11:29:26 +0900

    fix ci

--- .github/upload-artifacts.sh
+++ .github/upload-artifacts.sh
@@ -9,15 +9,17 @@ UPLOAD_DIR="${PROJECT_ROOT}/upload"
 mkdir -p "${UPLOAD_DIR}"
 
 # 1. JARファイルのコピー
+# JARファイルのコピー
 mkdir -p "${UPLOAD_DIR}/jars"
-cp "${PROJECT_ROOT}/bukkit/build/libs"/*.jar "${UPLOAD_DIR}/jars/MoripaFishing-bukkit-${COMMIT_SHORT_SHA}.jar"
-cp "${PROJECT_ROOT}/api/build/libs"/*.jar "${UPLOAD_DIR}/jars/MoripaFishing-api-${COMMIT_SHORT_SHA}.jar"
+cp "${PROJECT_ROOT}/bukkit/build/libs/bukkit-${COMMIT_HASH}.jar" "${UPLOAD_DIR}/jars/MoripaFishing-bukkit-${COMMIT_HASH}.jar"
+cp "${PROJECT_ROOT}/api/build/libs/api-${COMMIT_HASH}.jar" "${UPLOAD_DIR}/jars/MoripaFishing-api-${COMMIT_HASH}.jar"
 
-# 2. テスト結果のコピー
+# テスト結果のコピー
 mkdir -p "${UPLOAD_DIR}/junit"
 cp -r "${PROJECT_ROOT}/bukkit/build/reports/tests/test"/* "${UPLOAD_DIR}/junit/"
 
+# Detektのレポートのコピー
 mkdir -p "${UPLOAD_DIR}/detekt"
 cp -r "${PROJECT_ROOT}/build/reports/detekt"/* "${UPLOAD_DIR}/detekt/"
 
-aws s3 cp "${UPLOAD_DIR}" --endpoint-url "${S3_ENDPOINT}" "s3://${S3_UPLOAD_BUCKET}/${COMMIT_SHORT_SHA}" --recursive
+#aws s3 cp "${UPLOAD_DIR}" --endpoint-url "${S3_ENDPOINT}" "s3://${S3_UPLOAD_BUCKET}/${COMMIT_SHORT_SHA}" --recursive
