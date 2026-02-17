cc23b90f88c4bde400600c3cb96c6b5603f185fd
nlkomaru <nikomaru@nikomaru.dev>
2026-02-17 09:05:08 +0900

    GitHub Packages/JitPackからMaven Centralへ移行
    - vanniktech maven-publishプラグインでMaven Central (Central Portal)に公開
    - リリースワークフローにMaven Central publish stepを追加
    - preview workflowのJitPackトリガーはSNAPSHOT用に維持
    - api-guide.mdxをMaven Central + JitPack SNAPSHOT構成に更新
    - ライセンスをCC0 1.0に設定

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,21 +1,45 @@
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.serialization)
-    `maven-publish`
+    alias(libs.plugins.maven.publish)
 }
 
 group = "party.morino"
 version = project.version.toString()
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = group.toString()
-            artifactId = "moripafishing-api"
-            version = version
-            from(components["kotlin"])
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+
+    coordinates("party.morino", "moripafishing-api", version.toString())
+
+    pom {
+        name.set("MoripaFishing API")
+        description.set("API for MoripaFishing Minecraft fishing plugin")
+        url.set("https://github.com/morinoparty/MoripaFishing")
+
+        licenses {
+            license {
+                name.set("CC0 1.0 Universal")
+                url.set("https://creativecommons.org/publicdomain/zero/1.0/")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("morinoparty")
+                name.set("MorinoParty")
+                url.set("https://github.com/morinoparty")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/morinoparty/MoripaFishing")
+            connection.set("scm:git:git://github.com/morinoparty/MoripaFishing.git")
+            developerConnection.set("scm:git:ssh://git@github.com/morinoparty/MoripaFishing.git")
         }
     }
 }
